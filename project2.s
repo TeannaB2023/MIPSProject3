cd64486219fdb0cd24075b0d8de065e56be06acc
TeannaB2023 <teanna.barrett@bison.howard.edu>
2020-11-02 20:15:23 -0600

    Cleared majority of code to start from scratch

--- project2.s
+++ project2.s
@@ -7,6 +7,7 @@
 #
 # Arg registers used: $a0, $a1, $a2, $a3
 # Tmp registers used: $t0, $t1, $t2, $t3, $t4, $t5
+# Save registers used: $s0, $s1
 # Value registers used: $v0, $v1
 #
 # Pre: none
@@ -19,6 +20,7 @@
 	.data # This is the section to declare variables that will be used in the program
 	x: .space 1001 		# The space for the input is initialized (will only read 1000 characters endline)
 	id: .word 02924893	# Defines the value of Teanna Barrett's id
+	invalid: .asciiz "Invalid input" # Value that should be printed if the input is invalid
 
 	.text # This is the section where the instructions will be written
 main:
@@ -34,50 +36,9 @@ main:
 	div	$t5, $t6		# X % 11
 	mfhi	$t5			# Move the remainder of the division to a register
 	addi	$s0, $t5, 26		# N = (X % 11) + 26
-	jal	START
-	sh	$t0, 0($a0)	# Store the sum in memory right after the id string
 	li	$v0, 1		# Loads value that tells syscall to print
-	lh	$a0, 0($a0)	# Load the sum from memory so it can be printed
+	lw	$a0, 0($a0)	# Load the sum from memory so it can be printed
 	syscall			# Completes the print instruction
 
 	li	$v0, 10		# Exit program call
 	syscall		
-START:	
-	lb	$t1, 0($a0)		# Loads the ASCII value of the first character
-	slti	$t2, $t1, 48		# Evaluates if the ASCII value could be a number or letter
-	bne	$t2, $zero, NOT		# If the value of the character is less than it's not a viable number
-
-NUM:
-	slti	$t2, $t1, 58		# Checks if the value represents a number
-	beq	$t2, $zero, UPPER	# If not check to see if it's an uppercase letter
-	addi	$t1, $t1, -48		# Adjusts the value of number to base N
-	j ADD				# Add the value to sum
-
-UPPER:
-	slti	$t2, $t1, 91		# Checks if the value represents an uppercase letter
-	beq	$t2, $zero, LOWER	# If not check to see if it's a lowercase letter
-	slti	$t2, $t1, 65		# Checks the lower bound of the upper case 
-	bne	$t2, $zero, NOT		# If out of range it is not a viable character
-	addi	$t1, $t1, -55		# Adjusts the value of upper case letter to base N
-	j ADD				# Add the value to sum
-
-LOWER:
-	slti	$t2, $t1, 97		# Checks the lower bound of the lower case 
-	bne	$t2, $zero, NOT		# If out of range it is not a viable character
-	addi	$t1, $t1, -87		# Adjusts the value of lower case letter to base N
-	j ADD				# Add the value to sum
-
-NOT:					
-	add	$t1, $zero, $zero	# If not the sum is skipped over
-
-ADD:	
-	slt	$t2, $t1, $s0		# Checks if the converted value is less than the base number
-	beq	$t2, $zero, INCREMENT	# If the value cannot be represented by the base it's not added to the sum
-	add	$t0, $t0, $t1		# Adds the character value to the sum
-
-INCREMENT:
-	addi	$a0, $a0, 1		# Increment the address by one
-	addi	$t3, $t3, 1		# Increment the loop counter by 1
-	slti	$t2, $t3, 1000		# This will check if current place of the loop is still on the input 
-	bne	$t2, $zero, START	# The program will loop for only 1000 iterations		
-	jr $ra
